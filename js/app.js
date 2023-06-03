3e94ebdbe6e365923122365ce645b7bca2c31af8
oRix012 <70334905+RAMICHE639@users.noreply.github.com>
2023-06-03 16:07:24 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+
+
+var scene2 = document.getElementById('scene2');
+var parallaxInstance = new Parallax(scene2);
+
+
+var scene3 = document.getElementById('scene3');
+var parallaxInstance = new Parallax(scene3);
+
+
+var scene4 = document.getElementById('scene4');
+var parallaxInstance = new Parallax(scene4);
+
+
+var scene5 = document.getElementById('scene5');
+var parallaxInstance = new Parallax(scene5);
+
+
+var scene6 = document.getElementById('scene6');
+var parallaxInstance = new Parallax(scene6);
+
+
+var scene7 = document.getElementById('scene7');
+var parallaxInstance = new Parallax(scene7);
+
+
+var scene8 = document.getElementById('scene8');
+var parallaxInstance = new Parallax(scene8);
